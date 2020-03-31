8f81e62ab8cace8fe2eb099f5b2b73978fc71257
acb17rmh <rmhindes1@sheffield.ac.uk>
2020-03-31 16:29:53 +0100

    Finished refactoring classifier

--- cfp_classifier/cfp_classifier.py
+++ cfp_classifier/cfp_classifier.py
@@ -3,101 +3,112 @@ import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, f1_score, precision_score
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+import time
 
-"""
-Naive bayes classifier for CFP emails.
-@author: Richard Hindes
-@data: 18/11/2019
-@resource: labelled_data.csv - a csv file containing each email and its label
-"""
 
-"""
-@function preprocess_text
+class CFPClassifier():
 
-Given an input string, removes commas, punctuation and any words
-that are in the stoplist.
+    def __init__(self):
+        self.data_train, self.data_test = self.load_data("data/new_labelled_data.csv")
+        self.train_counts, self.test_counts = self.vectorize()
+        self.vectorizer
+        self.classifier = self.train_classifier()
 
-@param text - text as a string to be pre-processed
-@return text_words - a tokenised list of words in the input
-"""
+    def load_data(self, data, test_size=0.25):
+        """
+        Function to load a labelled dataset from a CSV file, and split it into a training set and a testing set.
 
+        Args:
+            data: the CSV file to be loaded
+            test_size: If float, should be between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split.
+                       If int, represents the absolute number of test samples.
+                       If None, the value is set to the complement of the train size.
+                       If train_size is also None, it will be set to 0.25.
+        Returns:
+            (DataFrame, DataFrame): a tuple of Pandas DataFrames, where the first DataFrame is the training data,
+                                    and the second DataFrame is the training data.
+        """
+        dataframe = pd.read_csv(data).head(10).dropna()
+        data_train, data_test = train_test_split(dataframe, test_size=test_size)
+        return data_train, data_test
+
+    def vectorize(self):
+        """
+        Function to train the classifier on the data provided.
+        Returns:
+            (sparse matrix, sparse matrix): a tuple of sparse matrices, where the first matrix is the document-term
+                                            matrix for the training data, and the second matrix is the document-term
+                                            matrix for the testing data.
+        """
+        vectorizer = CountVectorizer(analyzer=preprocess_text)
+        self.vectorizer = vectorizer
+        train_counts = vectorizer.fit_transform(self.data_train['text'])
+        test_counts = vectorizer.transform(self.data_test['text'])
+        return train_counts, test_counts
+
+    def train_classifier(self):
+        """
+        Function to train the classifier on the data provided.
+        Returns:
+            MultinomialNB: a trained instance of an sklearn MultinomialNB object.
+        """
+        classifier = MultinomialNB()
+        targets = self.data_train["class"]
+        classifier.fit(self.train_counts, targets)
+        return classifier
+
+    def classify_text(self, input_text_list):
+        """
+        Function to classify a set of input sets.
+        Args:
+            input_text_list: a list of strings to be classified.
+        Returns:
+            list: a list of predicted labels corresponding to the input list's elements.
+        """
+        input_counts = self.vectorizer.transform(input_text_list)
+        predictions = self.classifier.predict(input_counts)
+        return predictions
+
+    def evaluate(self):
+        """
+        Function to run the trained classifer on the test set of data and evaluate its performance.
+        Also exports the results of the evaluation to an HTML document, which is saved in the /results subfolder.
+        """
+        # Run the classifier on test set and report performance
+        test_counts, test_set = self.test_counts, self.data_test
+        predictions = self.classifier.predict(test_counts)
+        print(classification_report(test_set["class"], predictions))
+        print("ACCURACY: {:.2%}".format(accuracy_score(test_set["class"], predictions)))
+
+        # Add the new labels to the DataFrame and save as an HTML document
+        predictions_df = pd.DataFrame(predictions)
+        test_set["prediction"] = predictions_df.values
+        filename = "results/classifier_results{}.html".format(time.time())
+        test_set.to_html(filename)
+        print("Saved results to file {}".format(filename))
 
 def preprocess_text(text):
+    """
+    Function to preprocess input texts before being vectorized. Performs tokenisation and stopword removal,
+    and removes punctuation from the text.
+
+    Args:
+        text: the input text to be preprocessed
+    Returns:
+        list: a list of words in the text
+    """
+    # get the stoplist from the NLTK corpus
+    stoplist = (stopwords.words("english"))
     # uses NLTK to tokenise the input email
     text = word_tokenize(text)
     # removes any punctuation or words if they are in the stoplist
     text_words = [word for word in text if word not in stoplist and word not in string.punctuation]
     return text_words
 
-
-# get the stoplist from the NLTK corpus
-stoplist = (stopwords.words("english"))
-
-# load in the labelled data
-# TODO: ability to specify own dataset from the command line
-data = pd.read_csv('C:/Users/Richard/PycharmProjects/cfp_project/cfp_classifier/data/new_labelled_data.csv').head(100).dropna()
-
-# Splits the text into a feature vector where each feature is a word
-email_vectors = CountVectorizer(analyzer=preprocess_text).fit_transform(data['text'])
-
-"""
-Splits the data into 4 sets:
-data_training: training data
-data_test: testing data
-label_train: class labels for all the training data
-label_test: class labels for all the testing data
-test_size parameter specifies that 25% of the data is used for testing, 75% used for training
-"""
-
-data_train, data_test, label_train, label_test = train_test_split(email_vectors, data['class'], test_size=0.25,
-                                                                  random_state=0)
-
-
-# Initilialises the naive bayes classifier and supplies it with the training data
-# Which is then used to trjain the classifier
-
-# classifier = svm.SVC(gamma = "scale")
-classifier = MultinomialNB()
-classifier.fit(data_train, label_train)
-
-# Predict the class (cfp or email) of the testing data
-predictions = classifier.predict(data_test)
-
-# print accuracy, recall, precision and f-measure
-print("ACCURACY: " + str(accuracy_score(label_test, predictions)))
-print("RECALL: " + str(recall_score(label_test, predictions, pos_label="cfp")))
-print("PRECISION " + str(precision_score(label_test, predictions, pos_label="cfp")))
-print("F-MEASURE " + str(f1_score(label_test, predictions, pos_label="cfp")))
-
-# plot confusion matrix as heatmap
-confusion_matrix = confusion_matrix(predictions, label_test)
-print(confusion_matrix)
-
-# 10-fold cross validation score
-cross_validation_scores = cross_val_score(classifier, email_vectors, data['class'], cv=10)
-print(cross_validation_scores.mean())
-
-"""
-# load CFP csv
-test_df = pd.read_csv('../information_extraction/data/wikicfp_sorted.csv')
-test_df = test_df['description']
-vectorizer = CountVectorizer()
-test_dataset = vectorizer.transform(test_df)
-print (classifier.predict(test_dataset))
-"""
-
-"""
-# Code for plotting the confusion matrix
-axes = plt.subplot()
-sns.heatmap(confusion_matrix, annot=True, ax=axes, cmap="Blues", fmt="g")
-axes.set_xlabel('True labels')
-axes.set_ylabel('Predicted labels')
-axes.set_title('Confusion Matrix')
-axes.xaxis.set_ticklabels(['CFP', 'non CFP'])
-axes.yaxis.set_ticklabels(['CFP', 'non CFP'])
-plt.show()
-"""
+if __name__ == "__main__":
+    cfp_classifier = CFPClassifier()
+    cfp_classifier.evaluate()
