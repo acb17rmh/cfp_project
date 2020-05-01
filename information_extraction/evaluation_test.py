31045ce5249a94260e8af25852c0ffd79416576b
acb17rmh <rmhindes1@sheffield.ac.uk>
2020-05-01 11:26:02 +0100

    much faster evaluation

--- information_extraction/evaluation_test.py
+++ information_extraction/evaluation_test.py
@@ -1,31 +1,32 @@
-from __future__ import unicode_literals, print_function
-
-import dateparser
 import pandas
-import pprint
-import re
-import spacy
-from cfp import Cfp
-import time
 
-# Load extraction datas
+# Load extraction data
 results_df = pandas.read_csv('NEW_RESULTS/new_results1588289488.4452426.csv', encoding="latin-1")
+
+def eval_location(dataframe):
+    actual_loc = str(dataframe[0])
+    detected_loc = str(dataframe[1])
+
+    if actual_loc in detected_loc or detected_loc in actual_loc:
+        return True
+    return False
 
 results_df['correct_start_date'] = results_df['start_date'] == results_df['detected_start_date']
 results_df['correct_notification_due'] = results_df['notification_due'] == results_df['detected_notification_due']
 results_df['correct_submission_deadline'] = results_df['submission_deadline'] == results_df['detected_submission_deadline']
 results_df['correct_final_version_deadline'] = results_df['final_version_deadline'] == results_df['detected_final_version_deadline']
 
-
-
-
-
+results_df['correct_location'] = results_df[['location', 'detected_location']].apply(eval_location, axis=1)
+results_df['correct_conference_name'] = results_df[['name', 'detected_conference_name']].apply(eval_location, axis=1)
 
 number_of_records = len(results_df.index)
-
-
-
 print (number_of_records)
+
+
+print ("CONFERENCE NAME PERFORMANCE: \n CORRECT RECORDS {} \n ACCURACY {:.2%}"
+       .format(results_df.correct_conference_name.sum(), results_df.correct_conference_name.sum()/number_of_records))
+print ("LOCATION PERFORMANCE: \n CORRECT RECORDS {} \n ACCURACY {:.2%}"
+       .format(results_df.correct_location.sum(), results_df.correct_location.sum()/number_of_records))
 print ("START DATE PERFORMANCE: \n CORRECT RECORDS {} \n ACCURACY {:.2%}"
        .format(results_df.correct_start_date.sum(), results_df.correct_start_date.sum()/number_of_records))
 print ("SUBMISSION DEADLINE PERFORMANCE: \n CORRECT RECORDS {} \n ACCURACY {:.2%}"
