1886f775c121b68e934c3ca4da3da7face1a73ff
acb17rmh <rmhindes1@sheffield.ac.uk>
2020-05-01 11:36:59 +0100

    Added joint accuracies

--- information_extraction/evaluation_test.py
+++ information_extraction/evaluation_test.py
@@ -11,18 +11,18 @@ def eval_location(dataframe):
         return True
     return False
 
+# Write new columns for each record, True if detected information is correct else False
 results_df['correct_start_date'] = results_df['start_date'] == results_df['detected_start_date']
 results_df['correct_notification_due'] = results_df['notification_due'] == results_df['detected_notification_due']
 results_df['correct_submission_deadline'] = results_df['submission_deadline'] == results_df['detected_submission_deadline']
 results_df['correct_final_version_deadline'] = results_df['final_version_deadline'] == results_df['detected_final_version_deadline']
-
 results_df['correct_location'] = results_df[['location', 'detected_location']].apply(eval_location, axis=1)
 results_df['correct_conference_name'] = results_df[['name', 'detected_conference_name']].apply(eval_location, axis=1)
 
 number_of_records = len(results_df.index)
 print (number_of_records)
 
-
+# Print the number and accuracy of extraction for each piece of data
 print ("CONFERENCE NAME PERFORMANCE: \n CORRECT RECORDS {} \n ACCURACY {:.2%}"
        .format(results_df.correct_conference_name.sum(), results_df.correct_conference_name.sum()/number_of_records))
 print ("LOCATION PERFORMANCE: \n CORRECT RECORDS {} \n ACCURACY {:.2%}"
@@ -35,3 +35,11 @@ print ("NOTIFICATION DUE PERFORMANCE: \n CORRECT RECORDS {} \n ACCURACY {:.2%}"
        .format(results_df.correct_notification_due.sum(), results_df.correct_notification_due.sum()/number_of_records))
 print ("FINAL VERSION DEADLINE PERFORMANCE: \n CORRECT RECORDS {} \n ACCURACY {:.2%}"
        .format(results_df.correct_final_version_deadline.sum(), results_df.correct_final_version_deadline.sum()/number_of_records))
+
+# Print joint accuracies (key data and all data)
+print (sum((results_df['correct_start_date']) & (results_df['correct_location'] & results_df['correct_conference_name'] &
+                                                 results_df['correct_final_version_deadline'] &
+                                                 results_df['correct_notification_due'] &
+                                                 results_df['correct_submission_deadline'])))
+
+print (sum((results_df['correct_start_date']) & (results_df['correct_location'] & results_df['correct_conference_name'])))
